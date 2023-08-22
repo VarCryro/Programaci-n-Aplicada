0865ab847ca27c4e449a2f6a2c114b601a48c4bf
VarCryro <114629843+VarCryro@users.noreply.github.com>
2023-08-22 13:20:12 -0500

    Add files via upload

---
+++ Primeros_programas/Meals.py
@@ -0,0 +1,21 @@
+# We've defined the variable "meal" here to the name of the food we ate for breakfast!
+meal = "An english muffin"
+
+# Printing out breakfast
+print("Breakfast:")
+print(meal)
+
+# Now update meal to be lunch!
+meal = "A Dish of meat with mashed potatoes and vegetables"
+
+# Printing out lunch
+print("Lunch:")
+print(meal)
+
+# Now update the dinner what you'll eat
+meal = "A changua whit arepas and chocolate"
+# Now update "meal" to be dinner
+
+# Printing out dinner
+print("Dinner:")
+print(meal)
