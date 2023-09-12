c89f28cdf2a9f512a91b99ae891a4dd0f083e021
VarCryro <114629843+VarCryro@users.noreply.github.com>
2023-09-12 13:27:41 -0500

    Update dic1.py

--- Segundo_Corte/Diccionarios/dic1.py
+++ Segundo_Corte/Diccionarios/dic1.py
@@ -22,3 +22,24 @@ print(menu)
 animals_in_zoo = {"dinosaurs": 0}
 animals_in_zoo = {"dinosaurs": 0}
 print(animals_in_zoo)
+
+user_ids = {"teraCoder": 9018293, "proProgrammer": 119238}
+print(user_ids)
+user_ids.update({"theLooper": 138475, "stringQueen": 85739})
+print(user_ids)
+
+oscar_winners = {"Best Picture": "La La Land", "Best Actor": "Casey Affleck", "Best Actress": "Emma Stone", "Animated Feature": "Zootopia"}
+oscar_winners.update({"Supporting Actress": "Viola Davis"})
+oscar_winners["Animated Feature"] = "Pinocho"
+
+drinks = ["espresso", "chai", "decaf", "drip"]
+caffeine = [64, 40, 0, 120]
+zipped_drinks = zip(drinks, caffeine)
+drinks_to_caffeine = {key:value for key, value in zipped_drinks}
+print(drinks_to_caffeine)
+
+code = [1, 20, 50, 7]
+product = ["pollo", "bandeja", "gaseosa 350", "sopa"]
+zipped_code = zip(code, product)
+code_to_product = {key:value for key, value in zipped_code}
+print(code_to_product)
