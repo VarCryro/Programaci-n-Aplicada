646fbda0ba59a5524c25344de4bf768d0fcb69f5
VarCryro <114629843+VarCryro@users.noreply.github.com>
2023-09-12 13:06:10 -0500

    Add files via upload

---
+++ Segundo_Corte/Diccionarios/Untitled-1.py
@@ -0,0 +1,24 @@
+from tkinter import Menu, Menubutton
+
+
+sensors =  {"living room": 21, "kitchen": 23, "bedroom": 20, "pantry": 22}
+num_cameras = {"backyard": 6,  "garage": 2, "driveway": 1}
+
+print(sensors)
+
+translations = {"mountain":	"orod", "bread":	"bass", "friend":	"mellon", "horse":	"roch" }
+print(translations)
+
+children = {"von Trapp": ["Johannes", "Rosmarie", "Eleonore"] , "Corleone": ["Sonny", "Fredo", "Michael"]}
+
+animals_in_zoo = {"zebras": 8, "monkeys": 12}
+animals_in_zoo["dinosaurs"] =  0
+print(animals_in_zoo)
+
+menu = {"oatmeal": 3, "avocado toast": 6, "carrot juice": 5, "blueberry muffin": 2}
+print(menu)
+menu["cheesecake"] = 8
+print(menu)
+animals_in_zoo = {"dinosaurs": 0}
+animals_in_zoo = {"dinosaurs": 0}
+print(animals_in_zoo)
