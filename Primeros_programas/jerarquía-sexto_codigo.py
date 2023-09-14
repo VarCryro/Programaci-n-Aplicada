8b34d0eb7fb26ef895d3a09707f8956745037203
VarCryro <114629843+VarCryro@users.noreply.github.com>
2023-09-14 12:58:54 -0500

    Create jerarquía-sexto_codigo.py

---
+++ Primeros_programas/jerarquía-sexto_codigo.py
@@ -0,0 +1,7 @@
+# Sin paréntesis
+result1 = 5 + 2 * 3
+# Con paréntesis
+result2 = (5 + 2) * 3
+
+print(result1)  # Imprimirá 11 (sin paréntesis, la multiplicación tiene prioridad)
+print(result2)  # Imprimirá 21 (con paréntesis, primero se suma 5 y 2, luego se multiplica por 3)
