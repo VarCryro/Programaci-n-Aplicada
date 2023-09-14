32e4fdaa15b6d3ec99a6a7be2bed87aacdbf6336
VarCryro <114629843+VarCryro@users.noreply.github.com>
2023-09-14 13:33:28 -0500

    Update Try.py

--- Loops/Try.py
+++ Loops/Try.py
@@ -1,3 +1,6 @@
-#Evalua lo que esta dentro de la funcion y le da un valor verdadero o falso segun sea. 
-my_cool_variable = 7 + 8 == 11
+#Evalua lo que esta dentro de la funcion y le da un valor verdadero o falso segun sea.
+a = ("Introduzca un número")
+b = ("Introduzca otro número")
+c = ("Introduzca un resultado para evaluar")
+my_cool_variable = a + b == c
 print(my_cool_variable)
